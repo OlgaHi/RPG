f970f2a45097b708fd4729e282be2d599b8e21e1
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-04 14:47:00 -0800

    Should correctly build an object, test pass
    Co-Authored-by: Jonah Johansen <johansenjonah+git@gmail.com>
    Co-Authored-by: James Wyn <thisisjameswyn@gmail.com>

--- src/js/unitObject.js
+++ src/js/unitObject.js
@@ -1,6 +1,6 @@
 export default class UnitObject {
   constructor(health,damage) {
-    // this.health = health;
-    // this.damage = damage;
+    this.health = health;
+    this.damage = damage;
   }
 }
