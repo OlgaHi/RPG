0839e4ea45271a6d04d7d0549a7322161fbaebca
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-04 13:57:57 -0800

    Init files
    Co-Authored: Jonah Johansen <johansenjonah+git@gmail.com>
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import $ from 'jquery';
+
+
+
+//player object
+  //health value
+  //attack value
+  //armor value
+
+
+
+
+
+//enemy object
+  //health value
+  //attack value
+  //attack value
+
+
+//gamemanger
+  //let playerObj
+  //let 
